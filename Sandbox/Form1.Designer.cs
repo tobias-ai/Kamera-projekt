c690ca926c072b70ec802d60a1905099168a467b
Tobias Bejfalk <tobias.bejfalk@learnet.se>
2020-02-14 10:32:39 +0100

    Add project files.

---
+++ Sandbox/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Sandbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(126)))), ((int)(((byte)(116)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Mongolian Baiti", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(11, 600);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(324, 112);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Camera";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(126)))), ((int)(((byte)(116)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Mongolian Baiti", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(544, 600);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(324, 112);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Take Snapshot";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(11, 86);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(857, 507);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(856, 514);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(74)))), ((int)(((byte)(79)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Poor Richard", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox1.Location = new System.Drawing.Point(218, 17);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(423, 53);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "CAMGIRL2000";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(881, 725);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Sandbox/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+
+// Important: include the opencvsharp library in your code
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace Sandbox
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture capture;
+        Mat frame;
+        Bitmap image;
+        private Thread camera;
+        bool isCameraRunning = false;
+        private void CaptureCamera()
+        {
+            camera = new Thread(new ThreadStart(CaptureCameraCallback));
+            camera.Start();
+        }
+
+        public void CaptureCameraCallback()
+        {
+            try
+            {
+                frame = new Mat();
+                capture = new VideoCapture(0);
+                capture.Open(0);
+                if (capture.IsOpened())
+                {
+                    while (isCameraRunning)
+                    {
+                        capture.Read(frame);
+                        image = BitmapConverter.ToBitmap(frame);
+                        if (pictureBox1.Image != null)
+                        {
+                            pictureBox1.Image.Dispose();
+                        }
+                        pictureBox1.Image = image;
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("stop pressing!");
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            bool exists = Directory.Exists(@"C:\Users\Tobias.bejfalk\Desktop\Portfolio");
+            if (exists == false)
+            {
+                System.IO.Directory.CreateDirectory(@"C:\Users\Tobias.bejfalk\Desktop\Portfolio");
+                MessageBox.Show("Picture folder has been created on your Desktop");
+            }
+            else
+            {
+                this.Hide();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data source=CND825B8ZY\SQLEXPRESS;Initial Catalog=KnitDatabas; Integrated Security=True;");
+            con.Open();
+            try
+            {
+                if (textBox1.Text != "")
+                {
+                    SqlCommand cmd = new SqlCommand("select text from knittable", con);
+                    SqlDataReader da = cmd.ExecuteReader();
+
+                    while (da.Read())
+                    {
+                        textBox1.Text = da.GetValue(0).ToString();
+                    }
+                    con.Close();
+                }
+            }
+            catch
+            {
+                textBox1.Text = "CAMGIRL2000";
+            }
+            try
+            {
+                if (button1.Text.Equals("Start Camera"))
+                {
+                    CaptureCamera();
+                    button1.Text = "Stop Camera";
+                    isCameraRunning = true;
+
+                }
+                else
+                {
+                    capture.Release();
+                    button1.Text = "Start Camera";
+                    isCameraRunning = false;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("stop pressing");
+            }
+            button1.Enabled = false;
+            Thread.Sleep(500);
+            button1.Enabled = true;
+
+        }
+        private void Button2_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (isCameraRunning)
+                {
+                    System.IO.Directory.CreateDirectory(@"C:\Users\tobias.bejfalk\Desktop\Portfolio\Webcam");
+                    Bitmap snapshot = new Bitmap(pictureBox1.Image);
+
+                    snapshot.Save(string.Format(@"C:\Users\tobias.bejfalk\Desktop\Portfolio\Webcam\{0}.png", Guid.NewGuid()), ImageFormat.Png);
+
+                }
+                else
+                {
+                    MessageBox.Show("Image can't be taken if the Camera isn't active!");
+                }
+                {
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
